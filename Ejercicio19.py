a8f3c549c45e4c1b6e7e54d66e52f00e4c6e4f23
ángela <al_amartinezn@iessierradelasvillas.org>
2022-11-12 21:11:37 +0100

    Todos menos. 19 y 20 sin for

--- Ejercicio19.py
+++ Ejercicio19.py
@@ -1,20 +1,25 @@
 '''Realizar un ejemplo de menú, donde podemos escoger las distintas opciones
 hasta que seleccionamos la opción de “Salir”.'''
 
+#import itertools  # para rangos infinitos
 
 opcion=0
-print("Elige una opcion. Pulsa 0 para salir")
-salir=4
-for i in range(0,salir):
-    salir=4
-    print("elige una opcion")
+
+print("*****MENU*****")
+print("opcion1 pulsa1.")
+print("opcion2 pulsa2.")
+print("opcion3. pulsa3.")
+print("para salir. pulsa 0.")
+print("*********")
+opcion=(int)(input("elige una opcion\n"))
+
+while opcion!=0: 
+    print("*****MENU*****")
     print("opcion1 pulsa1.")
     print("opcion2 pulsa2.")
     print("opcion3. pulsa3.")
-    print("para salir. pulsa 4.")
+    print("para salir. pulsa 0.")
     print("*********")
-    opcion=(int)(input("opcion"))
-    opcion1=1
-    opcion2=2
-    opcion3=3
-    opcion4=4
+    opcion=(int)(input("elige una opcion\n"))
+   
+print("programa terminado")
--- Ejercicio20.py
+++ Ejercicio20.py
@@ -1,14 +1,17 @@
 '''Mostrar en pantalla los N primero número primos. Se pide por teclado la cantidad
 de números primos que queremos mostrar.'''
 
+
 num=0
 divisible=0
 
 primo=(int)(num**(1/2))
 primos=(int)(input("¿cuantos números primos quiere mostrar?"))
 
+vPrimos=[]
 
-for i in range (1, primos):
-    if (num%i==0):
-        divisible+=1
-print(divisible)
+for i in range (2, num):
+
+    if (i%divisible==0):
+
+print(vPrimos)
